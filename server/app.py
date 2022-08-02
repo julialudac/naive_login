1d8cf893004ade4b9901a058f8442214f9a3c5d8
Julia Lu Dac <julialudac@yahoo.fr>
2022-08-02 21:48:18 +0200

    server: add routes to display info and change them

--- server/app.py
+++ server/app.py
@@ -10,16 +10,43 @@ users = [
   }
 ]
 
-@app.route("/login", methods=['POST'])
-def index():
+def get_login_response(req):
   response = jsonify({})
   response.headers.add('Access-Control-Allow-Origin', '*')
 
-  req = request.get_json()
   user = next((x for x in users if x["username"] == req["username"]), None)
 
   if user is None or user["password"] != req["password"]:
-    return response, 401
+    return response, 401, None
+  return response, 200, user
+
+
+@app.route("/login", methods=['POST'])
+def index():
+  return get_login_response(request.get_json())
+
+
+@app.route("/profile", methods=['POST'])
+def profile():
+  response, status, user = get_login_response(request.get_json())
+  if status != 200:
+    return response, status
+
+  response = jsonify({"email": user["email"]})
+  response.headers.add('Access-Control-Allow-Origin', '*')
+  return response, 200
+
+
+@app.route("/edit", methods=["POST"])
+def edit():
+  req = request.get_json()
+  response, status, user = get_login_response(req)
+  if status != 200:
+    return response, status
+  if "new_email" not in req:
+    return response, 400
+
+  user["email"] = req["new_email"]
   return response, 200
 
 
