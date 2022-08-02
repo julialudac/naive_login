2c85afd2e090afca56336ca3e31342ab184ac29f
Julia Lu Dac <julialudac@yahoo.fr>
2022-08-02 23:36:37 +0200

    server: Remove password necessity

--- server/app.py
+++ server/app.py
@@ -5,30 +5,24 @@ app = Flask(__name__)
 users = [
   {
     "username": "test",
-    "password": "testp",
     "email": "test@gmail.com"
   }
 ]
 
-def get_login_response(req):
+def get_login_response(username):
   response = jsonify({})
   response.headers.add('Access-Control-Allow-Origin', '*')
 
-  user = next((x for x in users if x["username"] == req["username"]), None)
+  user = next((x for x in users if x["username"] == username), None)
 
-  if user is None or user["password"] != req["password"]:
+  if user is None:
     return response, 401, None
   return response, 200, user
 
 
-@app.route("/login", methods=['POST'])
-def index():
-  return get_login_response(request.get_json())
-
-
-@app.route("/profile", methods=['POST'])
-def profile():
-  response, status, user = get_login_response(request.get_json())
+@app.route("/profile/<username>")
+def profile(username):
+  response, status, user = get_login_response(username)
   if status != 200:
     return response, status
 
@@ -40,7 +34,7 @@ def profile():
 @app.route("/edit", methods=["POST"])
 def edit():
   req = request.get_json()
-  response, status, user = get_login_response(req)
+  response, status, user = get_login_response(req["username"])
   if status != 200:
     return response, status
   if "new_email" not in req:
