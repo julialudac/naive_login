e46868a49d6dc968555a52afda296cc563c7e04b
Julia Lu Dac <julialudac@yahoo.fr>
2022-08-03 01:03:55 +0200

    server: Use a db instead of fake data

--- server/app.py
+++ server/app.py
@@ -1,19 +1,26 @@
 from flask import Flask, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://ayomi:ayomi@localhost:81/ayomi"
 
-users = [
-  {
-    "username": "test",
-    "email": "test@gmail.com"
-  }
-]
+db = SQLAlchemy(app)
+
+class User(db.Model):
+  __tablename__="users"
+  _id = db.Column("id", db.Integer, primary_key=True)
+  username = db.Column(db.String(100))
+  email = db.Column(db.String(100))
+
+  def __init__(self, username, email):
+    self.username = username
+    self.email = email
 
 def get_login_response(username):
   response = jsonify({})
   response.headers.add('Access-Control-Allow-Origin', '*')
 
-  user = next((x for x in users if x["username"] == username), None)
+  user = User.query.filter_by(username=username).first()
 
   if user is None:
     return response, 401, None
@@ -26,7 +33,7 @@ def profile(username):
   if status != 200:
     return response, status
 
-  response = jsonify({"email": user["email"]})
+  response = jsonify({"email": user.email})
   response.headers.add('Access-Control-Allow-Origin', '*')
   return response, 200
 
@@ -40,9 +47,10 @@ def edit():
   if "new_email" not in req:
     return response, 400
 
-  user["email"] = req["new_email"]
+  user.email = req["new_email"]
   return response, 200
 
 
 if __name__ == "__main__":
+  db.create_all()
   app.run(debug=True, host='0.0.0.0')
